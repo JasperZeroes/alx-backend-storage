e2f739b71a88b126716480bc06607c7116a1dda5
jasperzeroes <jasperobed@gmail.com>
2024-10-18 14:48:37 +0100

    a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,5 +1,5 @@
 -- Creates a stored procedure ComputeAverageWeightedScoreForUser
--- that computes and store the average weighted score for a student.
+-- that computes and store the average weighted score for a.
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
